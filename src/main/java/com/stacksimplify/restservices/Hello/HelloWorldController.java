d0487b92c33174baa501bfb14366083c60a25127
danishfaridi <danish_faridi@outlook.com>
2025-05-27 16:35:59 +0530

    hello world controller

---
+++ src/main/java/com/stacksimplify/restservices/Hello/HelloWorldController.java
@@ -0,0 +1,23 @@
+package com.stacksimplify.restservices.Hello;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class HelloWorldController {
+    //Simple Method
+	// URI - /helloworld
+	//GET - either @request or @get mapping
+	
+	//@RequestMapping(method = RequestMethod.GET, path="/helloworld")
+	@GetMapping("/helloworld1")
+	public String helloWorld() {
+			return "Hello World!";
+	}
+	
+	@GetMapping("/helloworld-bean")
+	public UserDetails helloworldbean() {
+		return new UserDetails("danish", "Faridi", "Chennai");
+	}
+}
