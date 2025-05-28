992df2e13edffc1bf8d1b1a0ac03317ae2dbe36a
danishfaridi <danish_faridi@outlook.com>
2025-05-29 01:13:23 +0530

    JPA Base Rest services Complete.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,12 @@
+
+/** insert into users values(101, 'danish_faridi@outlook.com', 'Danish', 'Faridi', 'admin', 'ssn101', 'dfaridi');
+insert into users values(103, 'dmark@stacksimplify.com', 'David', 'Mark', 'admin', 'ssn103', 'dmark'); 
+insert into USERS values(102, 'gwiser@stacksimplify.com', 'Greg', 'Wiser', 'admin', 'ssn102', 'gwiser'); 
+insert into USERS values(103, 'dmark@stacksimplify.com', 'David', 'Mark', 'admin', 'ssn103', 'dmark'); 
+insert into USERS values(101, 'danish_faridi@outlook.com', 'Danish', 'Faridi', 'admin', 'ssn101', 'dfaridi');
+INSERT INTO users (ID, USER_NAME, FIRST_NAME, LAST_NAME, EMAIL_ADDRESS, ROLE, SSN) VALUES (1, 'john_doe', 'John', 'Doe', 'john.doe@example.com', 'ADMIN', '123-45-6789');
+**/
+
+insert into users values(101, 'danish_faridi@outlook.com', 'Danish', 'Faridi', 'admin', 'ssn101', 'dfaridi');
+insert into users values(102, 'gwiser@stacksimplify.com', 'Greg', 'Wiser', 'admin', 'ssn102', 'gwiser'); 
+insert into users values(103, 'dmark@stacksimplify.com', 'David', 'Mark', 'admin', 'ssn103', 'dmark'); 
